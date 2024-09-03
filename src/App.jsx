cd4ff6a722da4c1092511e26e11202038ba018ab
metalun4 <baris_aydemir@outlook.com>
2024-09-02 23:10:21 -0400

    Remove speed analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import {Main} from "./components/Main.jsx";
 import {Footer} from "./components/Footer.jsx";
 
+import { Analytics } from "@vercel/analytics/react"
+
 function App() {
   return (
       <>
         <Main/>
         <Footer/>
+        <Analytics/>
       </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SpeedInsights/>
-    <Analytics/>
     <App />
   </React.StrictMode>,
 )
